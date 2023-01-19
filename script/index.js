d3cc06fadbd8b6362bf185248f31adf505516391
Wutche Chikaome <wutchechikaome75@gmail.com>
2023-01-19 13:34:31 +0100

    added some javascript functions

--- script/index.js
+++ script/index.js
@@ -1,4 +1,12 @@
 /** @format */
+const navBtn = document.querySelector("#navbtn");
+const popupItems = document.querySelector("#popupitems");
+const btnClose = document.querySelector("#close");
 
+navBtn.addEventListener("click", () => {
+  popupItems.classList.add("displayitems")
+})
 
-
+btnClose.addEventListener("click", () => {
+  popupItems.classList.remove("displayitems")
+});
