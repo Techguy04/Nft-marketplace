de92bf452b29e4378859417d91b20a3d8c718a7d
Wutche Chikaome <wutchechikaome75@gmail.com>
2023-01-19 13:12:18 +0100

    added css effects

--- script/index.js
+++ script/index.js
@@ -1,19 +1,4 @@
 /** @format */
 
-const cardBx = document.querySelector("card");
-const cardBtn = document.querySelector(".btn");
-const cardPrice = document.querySelector(".price");
-const cardItems = document.querySelector(".carditems");
 
-
-cardBx.addEventListener("click", ()=> {
-  cardBtn.classList.add("cbtn")
-})
-// for (let i = 0; i < cardItems.length; i++) {
-//   if (cardItems[i].contains(cardBx)) {
-//     cardBx.addEventListener("mouseover", () => {
-//       cardBtn.style.display = "flex";
-//     })
-//   }
-// }
 
